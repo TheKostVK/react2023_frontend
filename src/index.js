fdb079ca6e71e2f1e9167a0b5514418d00cc687c
Vlad Kost <kostvkthekost@gmail.com>
2023-03-21 15:33:52 +0300

    v2.0.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,6 +9,8 @@ import "./index.scss";
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme";
 
+import store from './redux/store.js';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -15,7 +18,9 @@ root.render(
         <CssBaseline/>
         <ThemeProvider theme={theme}>
             <BrowserRouter>
-                <App/>
+                <Provider store={store}>
+                    <App/>
+                </Provider>
             </BrowserRouter>
         </ThemeProvider>
     </React.StrictMode>
---
+++ src/redux/slices/posts.js
@@ -0,0 +1,23 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const initialState = {
+    posts: {
+        items: [],
+        status: 'loading',
+    },
+    tags: {
+        items: [],
+        status: 'loading',
+    },
+};
+
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducer: {},
+});
+
+
+export const  postsReducer = postsSlice.reducer;
