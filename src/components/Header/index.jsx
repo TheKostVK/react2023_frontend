a8c381517992eb004e2d585a102e50d0df777e06
TheKost_ <thekost2@yandex.ru>
2023-03-29 11:11:13 +0300

    v2.0.0

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 import styles from "./Header.module.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Container from "@mui/material/Container";
 import { Link as RouterLink, useLocation } from "react-router-dom";
@@ -44,27 +46,40 @@ export const Header = () => {
           </div>
         </Container>
       </div>
-      {/*<div>*/}
-      {/*  <Breadcrumbs className={styles.buttons} aria-label="breadcrumb" style={{ margin: 10 }}>*/}
-      {/*    <Link component={RouterLink} color="inherit" to="/">*/}
-      {/*      Index*/}
-      {/*    </Link>*/}
-      {/*    {pathNames.map((name, index) => {*/}
-      {/*      const routeTo = `/${pathNames.slice(0, index + 1).join("/")}`;*/}
+      <div className="container py-5" style={{maxWidth: 1178}}>
+        <div className="row">
+          <div className="col">
+            <nav aria-label="breadcrumb" className="bg-light rounded-3 p-3 mb-3">
+              <ol className="breadcrumb mb-0">
+                <li className="breadcrumb-item">
+                  <Link component={RouterLink} color="inherit" to="/" style={{textDecoration: "none"}}>
+                    Home
+                  </Link>
+                </li>
+                {pathNames.map((name, index) => {
+                  const routeTo = `/${pathNames.slice(0, index + 1).join("/")}`;
 
-      {/*      return (*/}
-      {/*        <Link*/}
-      {/*          key={routeTo}*/}
-      {/*          component={RouterLink}*/}
-      {/*          color="inherit"*/}
-      {/*          to={routeTo}*/}
-      {/*        >*/}
-      {/*          {name}*/}
-      {/*        </Link>*/}
-      {/*      );*/}
-      {/*    })}*/}
-      {/*  </Breadcrumbs>*/}
-      {/*</div>*/}
+                  return (
+                    <li className="breadcrumb-item">
+                      <Link
+                      key={routeTo}
+                      component={RouterLink}
+                      color="inherit"
+                      to={routeTo}
+                      style={{textDecoration: "none"}}
+                    >
+                      {name}
+                    </Link>
+                    </li>
+                  );
+                })}
+                {/*<li className="breadcrumb-item"><a href="#">User</a></li>*/}
+                {/*<li className="breadcrumb-item active" aria-current="page">User Profile</li>*/}
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
